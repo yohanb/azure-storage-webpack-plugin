f01d5c00d6a37709bcec0536f0aff41395de5479
Yuri Belvalov <yohan.belval@gsoft.com>
2016-12-13 22:15:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+Azure Storage Webpack Plugin
+===================
+
+This is a [webpack](http://webpack.github.io/) plugin that allows you upload generated assets to an Azure storage account. 
+This uses the [azure-storage](https://www.npmjs.com/package/azure-storage) plugin to authenticate and upload to Azure.
+
+Maintainer: Yohan Belval [@yohanb](https://github.com/yohanb)
+
+Installation
+------------
+Install the plugin with npm:
+```shell
+$ npm install azure-storage-webpack-plugin --save-dev
+```
+ 
+Basic Usage
+-----------
+
+```javascript
+var AzureStorageWebpackPlugin = require('azure-storage-webpack-plugin');
+
+var webpackConfig = {
+  entry: 'index.js',
+  output: {
+    path: 'dist',
+    filename: 'bundle.js'
+  },
+  plugins: [new AzureStorageWebpackPlugin({
+    blobService: ['storageaccountname','key'],
+    container: { name: 'containername', options: { publicAccessLevel : 'blob' }}
+})]
+};
+```
+
+This will upload the `dist/bundle.js` file to the specified container.  
+Files contained in folders will also be uploaded following the respective folder structure.  
+
+**NOTE:** This plugin is not intented to be used when in a _hot-reloading_ Webpack setup.
+
+# License
+
+This project is licensed under MIT.
---
+++ index.js
@@ -0,0 +1,40 @@
+var azure = require('azure-storage');
+
+var utils = require('./lib/utils');
+
+function apply(options, compiler) {
+
+    // When assets are being emmited (not yet on file system)
+    compiler.plugin('emit', function (compilation, callback) {
+        
+        var blobService = azure.createBlobService.apply(azure, options.blobService);
+        blobService.createContainerIfNotExists(options.container.name,  options.container.options, function(error, result, response) {
+            if(!error){
+                var assets = utils.getAssets(compilation);
+                assets.forEach(function(asset) {
+                    blobService.createBlockBlobFromText(options.container.name, asset.filePath, asset.fileContent, function(error, result, response) {
+                    if(!error){
+                        console.log("successfully uploaded '" + asset.filePath + "to container '" + options.container.name + "'");
+                    } else {
+                        console.error(error);
+                    }
+                    });
+                });
+
+            } else {
+                console.error(error);
+            }
+        });
+    });
+}
+
+function AzureCdnDeployWebpackPlugin(options) {
+
+    // Simple pattern to be able to easily access plugin
+    // options when the apply prototype is called
+    return {
+        apply: apply.bind(this, options)
+    };
+}
+
+module.exports = AzureStorageDeployWebpackPlugin;
