c927260273a4839cd43daabde4e28bb68c70a6a7
Danny Guinther <dguinther@seismic.com>
2018-02-14 14:07:20 -0500

    Support uploading directory trees

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ var utils = require('./lib/utils');
 
 function apply(options, compiler) {
   // When assets are being emmited (not yet on file system)
-  compiler.plugin('emit', function (compilation, callback) {
+  compiler.plugin('after-emit', function (compilation, callback) {
     var blobService = azure.createBlobService.apply(azure, options.blobService);
     blobService.createContainerIfNotExists(options.container.name,  options.container.options, function(error, result, response) {
-      if(!error){
-        var assets = utils.getAssets(compilation);
-        assets.forEach(function(asset) {
-          blobService.createBlockBlobFromText(options.container.name, asset.filePath, asset.fileContent, { contentSettings: options.metadata }, function(error, result, response) {
-            if(!error){
-              console.log("successfully uploaded '" + asset.filePath + "' to container '" + options.container.name + "'");
-            } else {
+      if(error) {
+        console.error(error);
+        return;
+      }
+
+      function handleFiles(files) {
+        files.forEach(function(file) {
+          blobService.createBlockBlobFromLocalFile(options.container.name, file.name, file.path, { contentSettings: options.metadata }, function(error, result, response) {
+            if(error) {
               console.error(error);
+              return;
+            }
+
+            if (!process.env.SILENCE_UPLOADS) {
+              console.log("successfully uploaded '" + file.path + "' to container '" + options.container.name + "'");
             }
           });
         });
+      }
+
+      if (options.directory) {
+        utils.getDirectoryFilesRecursive(options.directory).then(handleFiles).then(callback);
       } else {
-        console.error(error);
+        handleFiles(utils.getAssetFiles(compilation));
+        callback();
       }
     });
   });
